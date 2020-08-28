f5dcae442d8c4ed7f5ed40ac47c348b2359be2dd
A01703125 <69862244+A01703125@users.noreply.github.com>
2020-08-28 09:06:01 -0500

    Add files via upload

---
+++ Proyecto Avance.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+print("Le gustaria comprar un coche?")
+answer = input("Responder si o no")
+if answer == "si"
+    print("Perfecto, qué coche quieres?")
+    input("Tiguan, Jetta o Golf
+    ")
+    if answer == "Tiguan"
+        
+    if answer == "Jetta"
+        
+    if answer == "Hatchback
+    
+else print("No te puedo ayudar")
